c46c40235b37f23b638ba1894873c8ccf501bba4
Eitan Reiffman <eitanreiffman@gmail.com>
2023-06-14 14:27:29 -0400

    Fixed CSS Bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,24 @@
 import AboutUs from "./pages/AboutUs";
 import Posts from "./pages/Posts";
+import SignUp from "./pages/SignUp";
 import WelcomePage from "./pages/WelcomePage";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 
 
 function App() {
+    
   return (
-    <Router>
-        <Routes>
-            <Route path="/" Component={WelcomePage} />
-            <Route path="/about" Component={AboutUs} />
-            <Route path="/posts" Component={Posts} />
-        </Routes>
-    </Router>
+    <div className="min-h-screen bg-slate-300">
+        <Router>
+            <Routes>
+                <Route path="/" Component={WelcomePage} />
+                <Route path="/about" Component={AboutUs} />
+                <Route path="/posts" Component={Posts} />
+                <Route path="/signup" Component={SignUp} />
+            </Routes>
+        </Router>
+    </div>
   );
 }
 
--- client/src/pages/WelcomePage.jsx
+++ client/src/pages/WelcomePage.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 
 export default function WelcomePage() {
   return (
-    <h1 className='text-blue-500 text-3xl font-bold'>Our React App Is Up And Running</h1>
+    <div>
+        <h1 className='text-blue-500 text-3xl font-bold'>Our React App Is Up And Running</h1>
+    </div>
   )
 }
