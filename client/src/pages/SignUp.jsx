569f6a61ef08a1b66591c75a7693223bbdd49c34
Eitan Reiffman <eitanreiffman@gmail.com>
2023-06-14 15:46:59 -0400

    Added Sign Up Form UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function SignUp() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
   return (
-    <form action="">
-        Sign Up Form
-    </form>
+    <div className="flex justify-center items-center h-screen">
+        <div className="max-w-xs p-4">
+            <h2 className="text-2xl font-bold mb-4 text-center">Sign Up</h2>
+            <form onSubmit={handleSubmit} >
+                <div className="mb-4">
+                    <label htmlFor="email" className='block mb-2 text-sm font-medium'>
+                        Email
+                    </label>
+                    <input 
+                        type="email" 
+                        id='email'
+                        value={email}
+                        onChange={handleEmailChange}
+                        className='w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500'
+                        required
+                    />
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="password" className='block mb-2 text-sm font-medium'>
+                        Password
+                    </label>
+                    <input 
+                        type="password" 
+                        id='password'
+                        value={password}
+                        onChange={handlePasswordChange}
+                        className='w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500'
+                        required
+                    />
+                </div>
+                <button
+                    type='submit'
+                    className='w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600'
+                >
+                    Sign Up
+                </button>
+            </form>
+        </div>
+    </div>
     )
 }
