dffd4e9580121088d49b6524872c147c0e947864
Eitan Reiffman <eitanreiffman@gmail.com>
2023-06-29 21:58:15 -0400

    Added features to Artist Setup UI

--- client/src/pages/ArtistSetup.jsx
+++ client/src/pages/ArtistSetup.jsx
@@ -3,15 +3,51 @@ import { useNavigate } from 'react-router-dom';
 
 export default function ArtistSetup() {
     const navigate = useNavigate();
+
+    const [options, setOptions] = useState([false, false, false, false, false]);
+
+    const handleOptionClick = (option) => {
+        if (option === 'musician') {
+            if (options[0] === false) {
+                setOptions((prevOptions) => [true, ...prevOptions.slice(1)]);
+            } else {
+                setOptions((prevOptions) => [false, ...prevOptions.slice(1)]);
+            }
+        } else if (option === 'composer') {
+            if (options[1] === false) {
+                setOptions((prevOptions) => [prevOptions[0], true, ...prevOptions.slice(2)]);
+            } else {
+                setOptions((prevOptions) => [prevOptions[0], false, ...prevOptions.slice(2)]);
+            }
+        } else if (option === 'producer') {
+            if (options[2] === false) {
+                setOptions((prevOptions) => [...prevOptions.slice(0,2), true, ...prevOptions.slice(3)]);
+            } else {
+                setOptions((prevOptions) => [...prevOptions.slice(0,2), false, ...prevOptions.slice(3)]);
+            }
+        } else if (option === 'audio engineer') {
+            if (options[3] === false) {
+                setOptions((prevOptions) => [...prevOptions.slice(0,3), true, prevOptions[4]]);
+            } else {
+                setOptions((prevOptions) => [...prevOptions.slice(0,3), false, prevOptions[4]]);
+            }
+        } else if (option === 'lyricist') {
+            if (options[4] === false) {
+                setOptions((prevOptions) => [...prevOptions.slice(0,4), true]);
+            } else {
+                setOptions((prevOptions) => [...prevOptions.slice(0,4), false]);
+            }
+        }
+    }
     
-    const [prompts, setPrompts] = useState([
+    const prompts = [
         "Would you like to create an artist profile?",
         "What type of artist would you classify yourself as?",
         "What genres are you interested in?",
         "Which instruments do you play?",
         "Write down a quick artist bio of yourself to tell us what you're all about",
         "You may link references to social media accounts if you like"
-    ]);
+    ];
 
     const [currentPromptIndex, setCurrentPromptIndex] = useState(0);
 
@@ -28,7 +64,7 @@ export default function ArtistSetup() {
         <>
             <div className="flex justify-center items-center h-screen">
                 <div>
-                    <h1 className='text-5xl font-bold'>{prompts[currentPromptIndex]}</h1>
+                    <h1 className='text-5xl py-4 font-bold'>{prompts[currentPromptIndex]}</h1>
                     {currentPromptIndex === 0 ? (
                     <div className='flex justify-around my-5 text-2xl'>
                         <button className='hover:text-gray-400' onClick={handleNextPrompt}>Yes, create artist profile</button>
@@ -36,7 +72,61 @@ export default function ArtistSetup() {
                     </div>
                     ) : currentPromptIndex === 1 ? (
                         <>
-                            <button onClick={handleNextPrompt}>Next</button>
+                            <div className='flex justify-around my-5 text-2xl'>
+                                <div
+                                    className={`p-2 rounded-2xl cursor-pointer ${
+                                        options[0] === true ?
+                                        'border border-blue-500 bg-blue-200'
+                                        : ''                                         
+                                    }`}
+                                    onClick={() => handleOptionClick('musician')}
+                                >
+                                    <span className={`${ options[0] === false ? 'hover:text-gray-400' : '' }`}>Musician</span>
+                                </div>
+                                <div
+                                    className={`p-2 rounded-2xl cursor-pointer ${
+                                        options[1] === true ?
+                                        'border border-blue-500 bg-blue-200'
+                                        : ''                                         
+                                    }`}
+                                    onClick={() => handleOptionClick('composer')}
+                                >
+                                    <span className={`${ options[1] === false ? 'hover:text-gray-400' : '' }`}>Composer</span>
+                                </div>
+                                <div
+                                    className={`p-2 rounded-2xl cursor-pointer ${
+                                        options[2] === true ?
+                                        'border border-blue-500 bg-blue-200'
+                                        : ''                                         
+                                    }`}
+                                    onClick={() => handleOptionClick('producer')}
+                                >
+                                    <span className={`${ options[2] === false ? 'hover:text-gray-400' : '' }`}>Producer</span>
+                                </div>
+                                <div
+                                    className={`p-2 rounded-2xl cursor-pointer ${
+                                        options[3] === true ?
+                                        'border border-blue-500 bg-blue-200'
+                                        : ''                                         
+                                    }`}
+                                    onClick={() => handleOptionClick('audio engineer')}
+                                >
+                                    <span className={`${ options[3] === false ? 'hover:text-gray-400' : '' }`}>Audio Engineer</span>
+                                </div>
+                                <div
+                                    className={`p-2 rounded-2xl cursor-pointer ${
+                                        options[4] === true ?
+                                        'border border-blue-500 bg-blue-200'
+                                        : ''                                         
+                                    }`}
+                                    onClick={() => handleOptionClick('lyricist')}
+                                >
+                                    <span className={`${ options[4] === false ? 'hover:text-gray-400' : '' }`}>Lyricist</span>
+                                </div>
+                            </div>
+                            <div className='flex justify-end'>
+                                <button onClick={handleNextPrompt} className='my-6 text-2xl'>Next</button>
+                            </div>
                         </>
                         ) : currentPromptIndex === 2 ? (
                         <>
